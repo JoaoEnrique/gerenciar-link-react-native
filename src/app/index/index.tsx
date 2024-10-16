548d2392031afd08120cb1a345f66e8e895f2296
joaoenrique <jebsantosalves@gmail.com>
2024-10-16 14:52:48 -0300

    feat: primeiro componente em react native

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -2,6 +2,8 @@ import { View, Image, TouchableOpacity } from "react-native"
 import { styles } from "./style"
 import { MaterialIcons } from "@expo/vector-icons"
 import { colors } from "@/styles/colors"
+import { Category } from "@/components/category"
+
 
 export default function Index(){
     return (
@@ -13,6 +15,8 @@ export default function Index(){
                     <MaterialIcons name="add" size={32} color={colors.green[300]}/>
                 </TouchableOpacity>
             </View>
+
+            <Category/>
         </View>
     )
 }
