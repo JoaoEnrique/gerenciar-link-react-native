7f2ab4c2826f8983598ba4b566092f943dfed647
joaoenrique <jebsantosalves@gmail.com>
2024-10-16 14:46:14 -0300

    feat: header

---
+++ src/app/index/index.tsx
@@ -0,0 +1,18 @@
+import { View, Image, TouchableOpacity } from "react-native"
+import { styles } from "./style"
+import { MaterialIcons } from "@expo/vector-icons"
+import { colors } from "@/styles/colors"
+
+export default function Index(){
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Image source={require('@/assets/logo.png')} style={styles.logo}/>
+
+                <TouchableOpacity>
+                    <MaterialIcons name="add" size={32} color={colors.green[300]}/>
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
+}
